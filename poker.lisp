ecd76dc0ea4b5ec541ffe8718c72cb3fd33f8c3c
Marco Herrero <hielo.helado@gmail.com>
2012-08-23 17:17:40 +0200

    Modulo base
    Actualizada la descripción, reparte y recoge eficientemente. No baraja
    aun.

--- poker.lisp
+++ poker.lisp
@@ -4,59 +4,97 @@
 
 ;;; Pretende ser un simulador de poker, construido modularmente, de manera que acepte diversas variedades del juego (Hold'em, Chicago, etc) mediante distintos modulos.
 
-;;; Modulos: Core, Apuestas, Hold'em.
+;;; Modulos: Core, Poker, Apuestas, Hold'em.
 
 ;;; CORE:
-;;;    Identifica lo basico de un juego de cartas, una carta, una baraja, una mano, jugadores, jugadas, ganador. Se puede "jugar" solo con este modulo, pero es un repartir y ver quien gana.
+;;;    Identifica lo basico de un juego de cartas, una carta, una baraja, una mano, jugadores. No se puede jugar solo con este modulo, ya que solo reparte, recoge y baraja.
 ;;; TODO:
 ;;;  Sistema de turnos (repartiendo una carta por turno).
 
-(defvar *jugadores* NIL)
+(defvar *jugadores* (make-hash-table))
 (defvar *baraja* NIL)
 
-;; Posiblemente tenga que anadir nombre aqui.
-(defun jugador (id mano)
-  (list id mano))
+;; Datos de un jugador. El id es lo unico obligatorio, el dinero empieza en 0.
+; Hand = mano, money = dinero para apostar, active = juega esa ronda
+(defun jugador (id &key name hand (money 0) (active T))
+  (list id name hand money active))
 
-;; Valor de 1 a 13 (11, 12 y 13 J, Q y K, respectivamente). 0-3 palos. Si esta mal formada, devuelve NIL.
+;; Crea un hashtable con num-jugadores, asignandoles su id como clave y los datos de (jugador) como valor.
+(defun crea-jugadores (num) 
+  (setf *jugadores* (make-hash-table))
+  ;; TODO Delete players.
+  ; Crea num-jugadores
+  (loop for n from 1 to num do
+       (setf (gethash n *jugadores*) (jugador n))
+       )
+  )
+
+;; Metodos auxiliares para consultar y modificar jugadores.
+(defun getAt (atributo jug)
+  (nth atributo (gethash jug *jugadores*)))
+(defun setAt (atributo jug data)
+  (setf (nth atributo (gethash jug *jugadores*)) data))
+
+;; Carta. Valor de 1 a 13 (11, 12 y 13 J, Q y K, respectivamente). 0-3 palos. Si esta mal formada, devuelve NIL.
 (defun carta (valor palo)
   (if (or (< valor 1) (> valor 13) (< palo 0) (> palo 3))
       NIL
       (list valor palo)))
 
-;; Mano de 5 cartas, si la mano esta mal formada, devuelve NIL
-(defun mano (a b c d e)
-  (if (or (equal a nil) (equal b nil) (equal c nil) (equal d nil) (equal e nil))
-      NIL
-      (list a b c d e)))
-
-;; Solo crea la baraja francesa, 52 cartas, ordenada.
+;; Crea la baraja francesa, 52 cartas, ordenada y elimina toda carta de la mano de todos los jugadores.
 (defun baraja ()
   (setf *baraja* NIL)
+  (loop for key being the hash-keys of *jugadores* do
+       (setf (nth 2 (gethash key *jugadores*)) NIL))
   (loop for palo from 0 to 3 do
        (loop for valor from 1 to 13 do
 	    (setf *baraja* (cons (carta valor palo) *baraja*))
        )
   )
   *baraja*)
-
+;; TODO
 ;; Mezcla la baraja.
 (defun shuffle ()
   ; Do nothing
 )
-
+;; TODO
 ;; Muestra la baraja en un formato bueno
 (defun print-baraja ())
 
+;; Saca 1 carta de la baraja
+(defun saca-carta ()
+  (let ((x (first *baraja*)))
+    (setf *baraja* (rest *baraja*))
+    x))
+
+;; Reparte 1 carta a cada jugador activo
+(defun reparte () 
+  (loop for key being the hash-keys of *jugadores* do
+       (if (nth 4 (gethash key *jugadores*))
+	   (setf (nth 2 (gethash key *jugadores*)) (cons (saca-carta) (nth 2 (gethash key *jugadores*)))))) 
+)
+
+
+
+;;;; MODULO POKER
+
+;; Devuelve una lista del tipo (x . mano) donde X es el valor de la mano (0-Nada - 9-Royal Flush), y la mano ordenada por importancia de cartas (p.ej: dobles parejas: pareja-alta, pareja-baja, carta)
 (defun comprueba-jugada (mano)
   )
 
-;  manos = (list mano1 mano2 ...)
+;;  Comprueba que jugador tiene la mano mas alta, y en caso de empate, quien tiene su primera carta mas alta (y despues segunda, etc).
+;; Puede generar empate.
 (defun comprueba-ganador (manos) 
 )
 
+;;; Funcion principal para jugar 1 partida. Para testear.
 (defun juega (jugadores)
-  ; Crea los jugadores y les asigna manos vacias.
-  (loop for jugador from 1 to jugadores do
-       (
+  ; Vacia jugadores antiguos, crea los jugadores y les asigna manos vacias.
+  (crea-jugadores num)
+  (setf *jugadores* NIL)
+  (loop for jug from 1 to jugadores do
+       (setf *jugadores* (cons (jugador jug (mano (list NIL))) *jugadores*)))
+  ; Reparte cartas (modulo basico, 5 cartas)
+  (reparte 5)
+)
        
